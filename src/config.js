dd9da18e5d631d5a1c720e2fca7cdf6fcf0d319c
matthadd <matthis.haddouche@gmail.com>
2021-12-10 18:24:34 +0100

    new commit

---
+++ src/config.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const path = require("path");
+const isLocal = typeof process.pkg === "undefined";
+const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
+const { MODE } = require(path.join(basePath, "src/blendMode.js"));
+const description = "Angry Bigfoots is the first deflationary NFT concept integrating a farm system with our $Frost Token, built to offer you a unique immersed metaverse experience";
+const baseUri = "ipfs://QmV1XCTj8XjkWeWm5YeVskjUYbyooxY6U2tB2iFz8fWhaT";
+
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 10000,
+    layersOrder: [
+      { name: "Background" },
+      { name: "Coat" },
+      { name: "Horn" },
+      { name: "Eyes" },
+      { name: "Mouth" },
+      { name: "Clothes" },
+    ],
+  },
+];
+
+const shuffleLayerConfigurations = false;
+
+const debugLogs = false;
+
+const format = {
+  width: 512,
+  height: 512,
+};
+
+const background = {
+  generate: true,
+  brightness: "80%",
+};
+
+const extraMetadata = {};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+const preview = {
+  thumbPerRow: 5,
+  thumbWidth: 50,
+  imageRatio: format.width / format.height,
+  imageName: "preview.png",
+};
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+};
